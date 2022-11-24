6ac7e173592d8865ae0eef3e27d2432fed2e4e1c
FrankMS <frank.mckenziestripp@hotmail.com>
2022-11-24 17:18:03 +1100

    Add inital working code for vic graph

--- static/js/vic.js
+++ static/js/vic.js
@@ -1,26 +1,26 @@
-// import Chart from 'chart.js/auto';
-const ctx = document.getElementById('myChart');
-// var vic_data = JSON.parse(document.getElementById("mydiv").dataset.myData);
-var vic_data = d3.json("/vicdata")
-console.log(vic_data)
+// // import Chart from 'chart.js/auto';
+// const ctx = document.getElementById('myChart');
+// // var vic_data = JSON.parse(document.getElementById("mydiv").dataset.myData);
+// var vic_data = d3.json("/vicdata")
+// console.log(vic_data)
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-      // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: 'My First Dataset',
-        data: vic_data,
-        // fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        // tension: 0.1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+// new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//       // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//       datasets: [{
+//         label: 'My First Dataset',
+//         data: vic_data,
+//         // fill: false,
+//         borderColor: 'rgb(75, 192, 192)',
+//         // tension: 0.1
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//       }
+//     }
+//   });
